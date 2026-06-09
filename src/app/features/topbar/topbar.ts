45715a00574fa2572472e2d6cef5bc1f690212c2
Guillermo Morán <tuahil@gmail.com>
2026-06-09 07:02:45 -0600

    add new lateral menu

--- src/app/features/topbar/topbar.ts
+++ src/app/features/topbar/topbar.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-topbar',
+  standalone: true,
   imports: [MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './topbar.html',
   styleUrls: ['./topbar.scss'],
 })
-export class Topbar {}
+export class Topbar {
+  isMenuOpen = signal(false);
+
+  toggleMenu() {
+    this.isMenuOpen.set(!this.isMenuOpen());
+  }
+
+  closeMenu() {
+    this.isMenuOpen.set(false);
+  }
+
+  importCurl(): void {
+    // placeholder for import cURL action
+    this.closeMenu();
+  }
+}
